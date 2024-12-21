6388f4a8dda08422e50452bbd0a76dc885100a8f
NeGnutiyKit <ngraboveckij@gmail.com>
2024-12-21 19:10:57 +0200

    hello

--- src/app/api/db/connect.ts
+++ src/app/api/db/connect.ts
@@ -7,7 +7,7 @@ const connectMongo = async () => {
     }
 
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI!);
         console.log("Connected to MongoDB");
     } catch (error) {
         console.error("MongoDB connection error:", error);
